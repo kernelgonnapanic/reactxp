a12ec2c9186b58e0d5cb7ca50245eec9428ad022
Anna Konopka <ania@zdwola.pl>
2018-10-31 10:54:51 +0100

    Final version

--- presentation/slideTitle.js
+++ presentation/slideTitle.js
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  Heading,
-  Slide,
-  Text
-} from "spectacle";
+import { Heading, Slide, Text } from "spectacle";
 
 export default () => (
   <Slide bgColor="primary">
@@ -12,6 +8,10 @@ export default () => (
     </Heading>
     <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
       how does react XP manage to unify mobile and web development
+    </Text>
+    <br />
+    <Text margin="10px 0 0" textColor="tertiary" textSize={20}>
+      Anna Konopka, Meet.js Summit 2018
     </Text>
   </Slide>
 );
